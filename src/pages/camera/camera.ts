a98bf28a881ac48870b68b54869384d62b2b8ff3
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-10-18 08:38:07 -0300

    Adicionando o ActionSheet na pagina da camera

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ActionSheetController
+} from 'ionic-angular';
 
 /**
  * Generated class for the CameraPage page.
@@ -17,7 +22,10 @@ export class CameraPage {
 
   myPhoto: string = '../../assets/imgs/logo.png'
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private actionSheet: ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -25,10 +33,36 @@ export class CameraPage {
   }
 
   choosePhoto () {
-    alert('Choose Photo')
+    let actionSheet = this.actionSheet.create({
+      title: 'Selecione uma Imagem',
+      buttons: [
+        {
+          text: 'Tirar Foto',
+          handler: () => {
+            this.takePhoto()
+          }
+        },
+        {
+          text: 'Selecione uma Foto',
+          handler: () => {
+            this.takePhoto()
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    })
+
+    actionSheet.present()
   }
 
   saveImage () {
     alert('Save Image')
+  }
+
+  private takePhoto () {
+
   }
 }
